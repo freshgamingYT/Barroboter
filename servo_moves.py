2a3c8ef814fea42b68d5a943d39c0bb3e368a656
Rene <Rene.Mlodoch@gmail.com>
2024-12-17 13:38:16 +0100

    Update servo_moves.py

--- servo_moves.py
+++ servo_moves.py
@@ -1,40 +1,34 @@
-from time import sleep
-import math
 import RPi.GPIO as GPIO
-
-
+from time import sleep
 
 class Servo_moves:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
     
+    pos: int = 0   # initial starting pos
+    steps: int = 200  # number of steps
+    usDelay: int = 950  # number of microseconds 
+    uS: float = 0.000001  # one microsecond
+    moving: bool = False
+
     def __init__(self, step_pin: int, direction_pin: int, enable_pin: int):
         self.step_pin = step_pin
         self.direction_pin = direction_pin
         self.enable_pin = enable_pin
-        
-        GPIO.setmode(GPIO.BCM)
+
+        print(f"Initializing GPIO: step_pin={self.step_pin}, direction_pin={self.direction_pin}, enable_pin={self.enable_pin}")
         GPIO.setup(self.step_pin, GPIO.OUT)
         GPIO.setup(self.direction_pin, GPIO.OUT)
         GPIO.setup(self.enable_pin, GPIO.OUT)
-
-        self.pos: int = 0   # initial starting pos
-        self.steps: int = 200  # number of steps
-        self.usDelay: int = 950  # number of microseconds 
-        self.uS: float = 0.000001  # one microsecond
-
         GPIO.output(self.enable_pin, GPIO.LOW)
 
     def set_step_length(self, steps: int) -> None:
         self.steps = steps
 
     def goRight(self, steps: int) -> None:
-        print('2')
         GPIO.output(self.direction_pin, GPIO.HIGH)  # Set direction to right
         for i in range(steps):
-            progress = i / steps
-
-            #sinus
-            delay_factor = 0.5 * (1 - math.cos(math.pi * progress))
-            delay = self.uS * self.usDelay * delay_factor
+            delay = self.uS * self.usDelay
 
             GPIO.output(self.step_pin, GPIO.HIGH)
             sleep(delay)
@@ -42,13 +36,9 @@ class Servo_moves:
             sleep(delay)
             
     def goLeft(self, steps: int) -> None:
-        GPIO.output(self.direction_pin, GPIO.LOW)  # Set direction to right
+        GPIO.output(self.direction_pin, GPIO.LOW)  # Set direction to left
         for i in range(steps):
-            progress = i / steps
-
-            #sinus
-            delay_factor = 0.5 * (1 - math.cos(math.pi * progress))
-            delay = self.uS * self.usDelay * delay_factor
+            delay = self.uS * self.usDelay
 
             GPIO.output(self.step_pin, GPIO.HIGH)
             sleep(delay)
@@ -60,3 +50,20 @@ class Servo_moves:
     
     def set_current_pos(self, value: int) -> None:
         self.pos = value
+
+    def start_moving(self, direction: str) -> None:
+        self.moving = True
+        if direction == 'left':
+            self._move_continuous(self.goLeft)
+        elif direction == 'right':
+            self._move_continuous(self.goRight)
+
+    def stop_moving(self) -> None:
+        self.moving = False
+
+    def _move_continuous(self, move_func) -> None:
+        while self.moving:
+            move_func(1)  # Move one step at a time
+
+    def cleanup(self) -> None:
+        GPIO.cleanup()
