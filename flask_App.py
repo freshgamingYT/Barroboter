02defc0af88c1382731af395901072e42d51cff0
Rene <Rene.Mlodoch@gmail.com>
2024-12-17 09:55:06 +0100

    Add files via upload

--- flask_App.py
+++ flask_App.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, jsonify, request
 # from flask_sqlalchemy import SQLAlchemy
-from servo import Servo
-from servo_moves import Servo_moves
-from Views import Views
+import servo
+import servo_moves
+# from views import Views
+import views
 
-class AppConfig:
-    servo_steps = {
+class Flask_App:
+    servo_steps: dict = {
         "pos1": 0,
         "pos2": 450,
         "pos3": 900,
@@ -23,24 +24,23 @@ class AppConfig:
     enable_pin = 27       # enable pin
 
     def __init__(self):
-        #self.db = SQLAlchemy()
-        #DB_NAME = 'ServoStep.db'
-        
         self.app = Flask(__name__)
         self.app.config['SECRET_KEY'] = 'Keins123!'
-        # self.app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-        self.db.init_app(self.app)
 
-        self.servo_moves = Servo_moves(step_pin=self.step_pin, 
+        self.servo_moves = servo_moves.Servo_moves(step_pin=self.step_pin, 
                                      direction_pin=self.direction_pin, 
                                      enable_pin=self.enable_pin)
-        self.servo = Servo(self.servo_steps, self.servo_moves)
-        self.views = Views(self.app, self.servo, self.servo_moves)
+        
+        self.servo = servo.Servo(self.servo_steps, 
+                           step_pin=self.step_pin, 
+                           direction_pin=self.direction_pin, 
+                           enable_pin=self.enable_pin)
+        self.views = views.Views(self.app, self.servo, self.servo_moves)
     
     def create_app(self):
         return self.app
 
 if __name__ == '__main__':
-    config = AppConfig()
+    config = Flask_App()
     app = config.create_app()
     app.run(debug=True)
