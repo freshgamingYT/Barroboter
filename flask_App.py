53a3a611d432a15e33d71f66eb13eabcc06c23dd
Rene <Rene.Mlodoch@gmail.com>
2025-01-08 09:48:23 +0100

    Update flask_App.py

--- flask_App.py
+++ flask_App.py
@@ -1,100 +1,61 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO
-from servo_moves import ServoMoves
 from servo import Servo
-import views
-import json
+from config import ConfigManager
+import threading
 
 class FlaskApp:
-    """
-    A class to represent the Flask application with SocketIO support.
+    def __init__(self, config_file: str):
+        self.config_manager = ConfigManager(config_file)
+        self.config_file = 'config.json'
 
-    Attributes
-    ----------
-    app : Flask
-        The Flask application instance.
-    socketio : SocketIO
-        The SocketIO instance for real-time communication.
-    servo_move : ServoMoves
-        The ServoMoves instance for controlling servo movements.
-    servo : Servo
-        The Servo instance for managing servo positions.
-    views : Views
-        The Views instance for setting up routes.
-
-    Methods
-    -------
-    __init__():
-        Initializes the FlaskApp instance, loads configuration, and sets up components.
-    load_config(filename: str) -> None:
-        Loads configuration from a JSON file.
-    create_app():
-        Returns the Flask application instance.
-    cleanup() -> None:
-        Cleans up resources.
-    """
-
-    def __init__(self):
-        """
-        Initializes the FlaskApp instance.
-
-        Sets up the Flask application, loads configuration, initializes
-        ServoMoves and Servo instances, and sets up routes.
-        """
         self.app = Flask(__name__)
-        self.app.config['SECRET_KEY'] = 'Keins123!'
+        self.app.config['SECRET_KEY'] = self.config_manager.get('secret_key')
         self.socketio = SocketIO(self.app)
 
-        # Load configuration from JSON file
-        self.load_config('config.json')
-
-        print("Initializing ServoMoves in FlaskApp")
-        self.servo_move = ServoMoves(step_pin=self.config['step_pin'], 
-                                     direction_pin=self.config['direction_pin'], 
-                                     enable_pin=self.config['enable_pin'],
-                                     socketio=self.socketio)
-        
         print("Initializing Servo in FlaskApp")
-        self.servo = Servo(positions=self.config['servo_steps'], 
-                           step_pin=self.config['step_pin'], 
-                           direction_pin=self.config['direction_pin'], 
-                           enable_pin=self.config['enable_pin'],
-                           socketio=self.socketio)
-        
-        self.views = views.Views(self.app, self.servo, self.servo_move, self.socketio)
-    
-    def load_config(self, filename: str) -> None:
-        """
-        Loads configuration from a JSON file.
+        self.servo = Servo(positions=self.config_manager.get('servo_steps'), 
+                           step_pin=self.config_manager.get('step_pin'), 
+                           direction_pin=self.config_manager.get('direction_pin'), 
+                           enable_pin=self.config_manager.get('enable_pin'),
+                           socketio=self.socketio,
+                           velocity_settings=self.config_manager.get('velocity_settings'),
+                           distance_thresholds=self.config_manager.get('distance_thresholds'),
+                           config_file=self.config_file)
 
-        Parameters
-        ----------
-        filename : str
-            The path to the JSON configuration file.
-        """
-        with open(filename, 'r') as f:
-            self.config = json.load(f)
+        self.setup_routes()
+
+    def setup_routes(self):
+        @self.app.route('/')
+        def index():
+            return render_template('index.html', step_count=self.servo.movement.get_current_pos())
+
+        @self.app.route('/current_position', methods=['GET'])
+        def current_position():
+            step_count = self.servo.movement.get_current_pos()
+            return jsonify(step_count=step_count)
 
     def create_app(self):
-        """
-        Returns the Flask application instance.
-
-        Returns
-        -------
-        Flask
-            The Flask application instance.
-        """
         return self.app
 
     def cleanup(self) -> None:
-        """
-        Cleans up resources by calling the cleanup method of ServoMoves.
-        """
-        self.servo_move.cleanup()
+        self.servo.cleanup()
+
+def console_input(servo):
+    while True:
+        step = input("Enter step (pos1, pos2, ..., pos10) or 'exit' to quit: ")
+        if step == 'exit':
+            break
+        servo.move_to(step)
 
 if __name__ == '__main__':
-    config = FlaskApp()
+    config_file = 'config.json'
+    config = FlaskApp(config_file)    
     app = config.create_app()
+
+    input_thread = threading.Thread(target=console_input, args=(config.servo,))
+    input_thread.start()
+
     try:
         config.socketio.run(app, debug=True)
     finally:
