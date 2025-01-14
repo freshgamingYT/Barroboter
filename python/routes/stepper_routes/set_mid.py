e55be2b2e2a70d16f0b3bc2cf159e6856e455a45
Rene <Rene.Mlodoch@gmail.com>
2025-01-14 14:37:28 +0100

    Create set_mid.py

---
+++ python/routes/stepper_routes/set_mid.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, request, jsonify
+import logging
+
+logger = logging.getLogger('my_logger')
+
+set_mid_route = Blueprint('set_mid_route', __name__)
+
+@set_mid_route.route('/set-mid', methods=['POST'])
+def set_mid():
+    data = request.get_json()
+    logger.debug(f'Received set_mid command: {data}')
