e408917eef29360b76b1a8f7c0819f8bfe6df779
Rene <Rene.Mlodoch@gmail.com>
2025-01-17 09:55:23 +0100

    Update fileHandler.py

--- code/fileHandler.py
+++ code/fileHandler.py
@@ -1,1 +1,20 @@
-empty
+import json
+from logger import setup_logger
+
+class FileHandler:
+    def __init__(self, filePath):
+        self.logger = setup_logger()
+        self.filePath = filePath
+        self.data = self.readJson()
+        
+    def readJson(self):
+        with open(self.filePath, 'r') as file:
+            data = json.load(file)
+            self.logger.debug("successfully loaded JsonFile for Positions")
+        return data
+    
+    def updateJson(self, newValue):
+        self.data["Endpoint"] = newValue
+        with open(self.filePath, 'w') as file:
+            json.dump(self.data, file, indent=4)
+            self.logger.debug("Successfully updated Endpoint with new Endpoint")
